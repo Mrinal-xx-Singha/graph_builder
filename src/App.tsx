59086e6127caf7b52b3ef48a943f6472b1862fa5
Mrinal Singha <mrinalsingha17@gmail.com>
2026-06-10 17:14:02 +0000

    chore: initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+const App = () => {
+  return (
+    <div
+      className="flex items-center justify-center h-screen text-4xl font-bold"
+    >
+
+      <h1>Add Graph Builder</h1>
+      <Button
+        className="ml-4"
+      >Click Me</Button>
+    </div>
+  )
+}
+
+export default App
