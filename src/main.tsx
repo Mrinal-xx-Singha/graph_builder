aea8e08dd373dcfd17a45a0ba24b5f942aa9940f
Mrinal Singha <mrinalsingha17@gmail.com>
2026-06-16 12:18:46 +0000

    chore: deployment

--- src/main.tsx
+++ src/main.tsx
@@ -11,14 +11,15 @@ const queryClient = new QueryClient()
 
 
 async function enableMocking() {
-  if (!import.meta.env.DEV) {
-    return
-  }
+  //!  Production 
+  // if (!import.meta.env.DEV) {
+  //   return
+  // }
 
   const { worker } = await import('./mocks/browser.ts')
   // worker.start returns a promise that resolves once the service worker is up and ready to intercept requests
 
-  return worker.start()
+  return worker.start({onUnhandledRequest:'bypass'})
 }
 
 
